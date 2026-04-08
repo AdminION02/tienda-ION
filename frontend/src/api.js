61e6c13465459e1225160b50047562e98742a0a6
AdminION02 <adminion2026@gmail.com>
2026-04-08 17:38:08 -0500

    descripción del cambio

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://tienda-ion.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://tienda-ion.onrender.com'
 });
 
 // Interceptor para agregar token en cada petición
